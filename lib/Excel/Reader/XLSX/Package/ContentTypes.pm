fe023f03c2ceaba4326447b4bec7fb7a34253210
John McNamara <jmcnamara@cpan.org>
2012-01-03 20:32:08 +0000

    Initial tests for ContentTypes parser.

---
+++ lib/Excel/Reader/XLSX/Package/ContentTypes.pm
@@ -0,0 +1,212 @@
+package Excel::Reader::XLSX::Package::ContentTypes;
+
+###############################################################################
+#
+# ContentTypes - A class for reading the Excel XLSX ContentTypes.xml file.
+#
+# Used in conjunction with Excel::Reader::XLSX
+#
+# Copyright 2012, John McNamara, jmcnamara@cpan.org
+#
+# Documentation after __END__
+#
+
+# perltidy with the following options: -mbl=2 -pt=0 -nola
+
+use 5.008002;
+use strict;
+use warnings;
+use Exporter;
+use Carp;
+use XML::LibXML::Reader;
+
+our @ISA     = qw(Exporter);
+our $VERSION = '0.00';
+
+
+###############################################################################
+#
+# new()
+#
+# Constructor.
+#
+sub new {
+
+    my $class = shift;
+
+    my $self = {
+        _reader       => undef,
+        _files        => {},
+    };
+
+    bless $self, $class;
+
+    return $self;
+}
+
+
+##############################################################################
+#
+# _read_file()
+#
+# Create an XML::LibXML::Reader instance from a file.
+#
+sub _read_file {
+
+    my $self     = shift;
+    my $filename = shift;
+
+    my $xml_reader = XML::LibXML::Reader->new( location => $filename );
+
+    $self->{_reader} = $xml_reader;
+
+    return $xml_reader;
+}
+
+
+##############################################################################
+#
+# _read_string()
+#
+# Create an XML::LibXML::Reader instance from a string. Used mainly for
+# testing.
+#
+sub _read_string {
+
+    my $self   = shift;
+    my $string = shift;
+
+    my $xml_reader = XML::LibXML::Reader->new( string => $string );
+
+    $self->{_reader} = $xml_reader;
+
+    return $xml_reader;
+}
+
+
+##############################################################################
+#
+# _read_filehandle()
+#
+# Create an XML::LibXML::Reader instance from a filehandle. Used mainly for
+# testing.
+#
+sub _read_filehandle {
+
+    my $self       = shift;
+    my $filehandle = shift;
+
+    my $xml_reader = XML::LibXML::Reader->new( IO => $filehandle );
+
+    $self->{_reader} = $xml_reader;
+
+    return $xml_reader;
+}
+
+
+##############################################################################
+#
+# _read_all_nodes()
+#
+# Read all the nodes of a ContentTypes.xml file using an XML::LibXML::Reader
+# instance.
+#
+sub _read_all_nodes {
+
+    my $self = shift;
+
+    while ( $self->{_reader}->read() ) {
+        $self->_read_node( $self->{_reader} );
+    }
+}
+
+
+##############################################################################
+#
+# _read_node()
+#
+# Callback function to read the <Types> attributes of the ContentTypes file.
+#
+sub _read_node {
+
+    my $self = shift;
+    my $node = shift;
+
+    # Only read the Override nodes.
+    return unless $node->name eq 'Override';
+
+
+    my $part_name    = $node->getAttribute('PartName');
+    my $content_type = $node->getAttribute('ContentType');
+
+
+    if ( $part_name =~ /app\.xml$/ ) {
+        $self->{_files}->{_app} = $part_name;
+        return;
+    }
+
+    if ( $part_name =~ /core\.xml$/ ) {
+        $self->{_files}->{_core} = $part_name;
+        return;
+    }
+
+    if ( $part_name =~ /sharedStrings\.xml$/ ) {
+        $self->{_files}->{_shared_strings} = $part_name;
+        return;
+    }
+
+    if ( $part_name =~ /styles\.xml$/ ) {
+        $self->{_files}->{_styles} = $part_name;
+        return;
+    }
+
+    if ( $part_name =~ /workbook\.xml$/ ) {
+        $self->{_files}->{_workbook} = $part_name;
+        return;
+    }
+
+    if ( $part_name =~ /sheet\d+\.xml$/ ) {
+        push @{ $self->{_files}->{_worksheets} }, $part_name;
+        return;
+    }
+}
+
+
+1;
+
+
+__END__
+
+=pod
+
+=head1 NAME
+
+ContentTypes - A class for reading the Excel XLSX ContentTypes.xml file.
+
+=head1 SYNOPSIS
+
+See the documentation for L<Excel::Reader::XLSX>.
+
+=head1 DESCRIPTION
+
+This module is used in conjunction with L<Excel::Reader::XLSX>.
+
+=head1 AUTHOR
+
+John McNamara jmcnamara@cpan.org
+
+=head1 COPYRIGHT
+
+© MMXII, John McNamara.
+
+All Rights Reserved. This module is free software. It may be used, redistributed and/or modified under the same terms as Perl itself.
+
+=head1 LICENSE
+
+Either the Perl Artistic Licence L<http://dev.perl.org/licenses/artistic.html> or the GPL L<http://www.opensource.org/licenses/gpl-license.php>.
+
+=head1 DISCLAIMER OF WARRANTY
+
+See the documentation for L<Excel::Reader::XLSX>.
+
+=cut
---
+++ t/package/content_types/content_types01.t
@@ -0,0 +1,52 @@
+###############################################################################
+#
+# Tests for Excel::Reader::XLSX::Package::ContentTypes methods.
+#
+# reverse('(c)'), January 2012, John McNamara, jmcnamara@cpan.org
+#
+
+use lib 't/lib';
+use TestFunctions qw(_is_deep_diff);
+use strict;
+use warnings;
+use Excel::Reader::XLSX::Package::ContentTypes;
+
+use Test::More tests => 1;
+
+###############################################################################
+#
+# Tests setup.
+#
+my $expected;
+my $caption;
+my $got;
+my $reader = Excel::Reader::XLSX::Package::ContentTypes->new();
+
+$reader->_read_filehandle( *DATA );
+$reader->_read_all_nodes();
+
+
+
+###############################################################################
+#
+# Test the _files property.
+#
+$caption = " \tContentTypes: _strings";
+
+$expected = {
+    '_workbook'       => '/xl/workbook.xml',
+    '_app'            => '/docProps/app.xml',
+    '_styles'         => '/xl/styles.xml',
+    '_worksheets'     => [ '/xl/worksheets/sheet1.xml' ],
+    '_core'           => '/docProps/core.xml',
+    '_shared_strings' => '/xl/sharedStrings.xml'
+};
+
+
+$got = $reader->{_files};
+
+_is_deep_diff( $got, $expected, $caption );
+
+__DATA__
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<Types xmlns="http://schemas.openxmlformats.org/package/2006/content-types"><Default Extension="rels" ContentType="application/vnd.openxmlformats-package.relationships+xml"/><Default Extension="xml" ContentType="application/xml"/><Override PartName="/docProps/app.xml" ContentType="application/vnd.openxmlformats-officedocument.extended-properties+xml"/><Override PartName="/docProps/core.xml" ContentType="application/vnd.openxmlformats-package.core-properties+xml"/><Override PartName="/xl/sharedStrings.xml" ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.sharedStrings+xml"/><Override PartName="/xl/styles.xml" ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.styles+xml"/><Override PartName="/xl/theme/theme1.xml" ContentType="application/vnd.openxmlformats-officedocument.theme+xml"/><Override PartName="/xl/workbook.xml" ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet.main+xml"/><Override PartName="/xl/worksheets/sheet1.xml" ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.worksheet+xml"/></Types>
