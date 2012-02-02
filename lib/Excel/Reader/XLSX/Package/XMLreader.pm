8ce9d45efbe407652e6034fd346043f251c8ab9f
John McNamara <jmcnamara@cpan.org>
2012-02-02 20:15:16 +0000

    Refactored out XML reader into parent class.

---
+++ lib/Excel/Reader/XLSX/Package/XMLreader.pm
@@ -0,0 +1,158 @@
+package Excel::Reader::XLSX::Package::XMLreader;
+
+###############################################################################
+#
+# XMLreader - A class for reading Excel XLSX XML files.
+#
+# Used in conjunction with Excel::Reader::XLSX
+#
+# Copyright 2012, John McNamara, jmcnamara@cpan.org
+#
+# Documentation after __END__
+#
+
+# perltidy with the following options: -mbl=2 -pt=0 -nola
+
+use 5.008002;
+use strict;
+use warnings;
+use Exporter;
+use Carp;
+use XML::LibXML::Reader;
+
+our @ISA     = qw(Exporter);
+our $VERSION = '0.00';
+
+
+###############################################################################
+#
+# new()
+#
+# Constructor.
+#
+sub new {
+
+    my $class = shift;
+
+    my $self = { _reader => undef, };
+
+    bless $self, $class;
+
+    return $self;
+}
+
+
+##############################################################################
+#
+# _read_file()
+#
+# Create an XML::LibXML::Reader instance from a file.
+#
+sub _read_file {
+
+    my $self     = shift;
+    my $filename = shift;
+
+    my $xml_reader = XML::LibXML::Reader->new( location => $filename );
+
+    $self->{_reader} = $xml_reader;
+
+    return $xml_reader;
+}
+
+
+##############################################################################
+#
+# _read_string()
+#
+# Create an XML::LibXML::Reader instance from a string. Used mainly for
+# testing.
+#
+sub _read_string {
+
+    my $self   = shift;
+    my $string = shift;
+
+    my $xml_reader = XML::LibXML::Reader->new( string => $string );
+
+    $self->{_reader} = $xml_reader;
+
+    return $xml_reader;
+}
+
+
+##############################################################################
+#
+# _read_filehandle()
+#
+# Create an XML::LibXML::Reader instance from a filehandle. Used mainly for
+# testing.
+#
+sub _read_filehandle {
+
+    my $self       = shift;
+    my $filehandle = shift;
+
+    my $xml_reader = XML::LibXML::Reader->new( IO => $filehandle );
+
+    $self->{_reader} = $xml_reader;
+
+    return $xml_reader;
+}
+
+
+##############################################################################
+#
+# _read_all_nodes()
+#
+# Read all the nodes of an Excel XML file using an XML::LibXML::Reader
+# instance. Sub-classes will provide the _read_node() method.
+#
+sub _read_all_nodes {
+
+    my $self = shift;
+
+    while ( $self->{_reader}->read() ) {
+        $self->_read_node( $self->{_reader} );
+    }
+}
+
+
+1;
+
+
+__END__
+
+=pod
+
+=head1 NAME
+
+XMLreader - A class for reading Excel XLSX XML files.
+
+=head1 SYNOPSIS
+
+See the documentation for L<Excel::Reader::XLSX>.
+
+=head1 DESCRIPTION
+
+This module is used in conjunction with L<Excel::Reader::XLSX>.
+
+=head1 AUTHOR
+
+John McNamara jmcnamara@cpan.org
+
+=head1 COPYRIGHT
+
+© MMXII, John McNamara.
+
+All Rights Reserved. This module is free software. It may be used, redistributed and/or modified under the same terms as Perl itself.
+
+=head1 LICENSE
+
+Either the Perl Artistic Licence L<http://dev.perl.org/licenses/artistic.html> or the GPL L<http://www.opensource.org/licenses/gpl-license.php>.
+
+=head1 DISCLAIMER OF WARRANTY
+
+See the documentation for L<Excel::Reader::XLSX>.
+
+=cut
